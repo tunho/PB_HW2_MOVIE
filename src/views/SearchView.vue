03a34bb6188e7f6ce8a2009f17e67d374911ff08
Lee-Jun-Ho <tjens2322@naver.com>
2025-12-14 22:00:56 +0900

    style: adjust table view row counts (search: 8, popular: 12)

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -76,7 +76,7 @@
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="movie in filteredMovies" :key="movie.id">
+                <tr v-for="movie in filteredMovies.slice(0, 8)" :key="movie.id">
                   <td>
                     <img :src="getImageUrl(movie.poster_path, 'w92')" alt="poster" class="table-poster" />
                   </td>
